780e4b59fc714b5839dcf3fd88bc07c727437ebb
Neil Opena <neil1114o@gmail.com>
2018-07-31 17:13:53 -0400

    Transferred data to database

--- BigNerdRanch/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ BigNerdRanch/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -104,6 +104,7 @@ public class CrimeListFragment extends Fragment{
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         }else{
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -224,6 +225,10 @@ public class CrimeListFragment extends Fragment{
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes){
+            mCrimes = crimes;
         }
     }
 }
